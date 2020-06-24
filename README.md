27930df0ac13c5eb97b745c99d86b6f43294d1bb
NIE <bloodymilk1337@gmail.com>
2020-06-24 21:47:53 +0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # SwiftUIChatFB
-Simple test chat app with Swift UI 2 and Swift 5.3
+Simple test chat app with Swift UI 2 and XCode 12 (Swift 5.3)
---
+++ SwiftUIChatFB/SwiftUIChatFB/SwiftUIChatFBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatFBApp.swift
+//  SwiftUIChatFB
+//
+//  Created by Игорь on 24.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatFBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
